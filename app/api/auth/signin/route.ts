0745044fb1e692f459438a61208520b2cf183702
jack9801 <119859548+jack9801@users.noreply.github.com>
2025-04-11 04:16:17 +0530

    Update route.ts

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import * as bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export async function POST(request: Request) {
   try {
